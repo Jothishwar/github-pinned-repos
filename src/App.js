f8ff65167346c7252d51363fd0a886e6c8af4737
jothishwar <jothishwarvvr@gmail.com>
2023-01-21 22:03:00 +0530

    fetch repositories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [user] = useState('jothishwar');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      const res = await fetch(`https://api.github.com/users/${user}/repos?page=1&per_page=10&sort=created`)
+      const data = await res.json()
+      setItems(data)
+    };
+    fetchRepos()
+  },[])
+
   return (
     <div className="App">
       Github User
+      {items && (<div>
+        <h2>Repositories</h2>
+        <div>
+          {items.map((item)=>{
+            <Card key={item.map} data={item} />
+          })}
+        </div>
+      </div>)}
     </div>
   );
 }
