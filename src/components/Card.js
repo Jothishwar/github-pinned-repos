b4c7da387e4651b30a587e4a51b600e68779e378
jothishwar <jothishwarvvr@gmail.com>
2023-01-22 15:55:17 +0530

    added description and style

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,10 +16,17 @@ function Card({data,user}) {
 				</div>
 				<div className='repo_details'>
 					<h4>{data.owner}</h4>
-					<h5>{data.repo}</h5>
+					<a href={data.link} target="__blank" >
+						<h5>{data.repo}</h5>
+						<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+						  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+						  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+						</svg>
+					</a>
 				</div>
 			</div>
 			<div className='card_body'>
+				<p>{data.description}</p>
 				<div className='data'>
 					<h5>Language : </h5>
 					<p style={lang_style}></p> 
@@ -37,7 +44,7 @@ function Card({data,user}) {
 					<h5>Live Site : </h5>
 					<a href={`https://${user}.github.io/${data.repo}/`} target='__blank' >
 						<p>click here </p>
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+						<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
 						  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
 						  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
 						</svg>
@@ -48,4 +55,4 @@ function Card({data,user}) {
 	)
 }
 
-export default Card
+export default Card;
