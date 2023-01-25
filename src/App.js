51fc744d3160d7dd75d4e61d0345a8629e4b2ba3
jothishwar <jothishwarvvr@gmail.com>
2023-01-25 21:57:13 +0530

    added input to change user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import Card from './components/Card';
+import { Button } from "@chakra-ui/button";
 
 function App() {
 
-  const [user] = useState('jothishwar');
+  const [user,setUser] = useState('jothishwar');
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    const fetchRepos = async () => {
+  const fetchData= () => {
+      const fetchRepos = async () => {
       const res = await fetch(`https://gh-pinned-repos.egoist.dev/?username=${user}`)
       const data = await res.json()
       setItems(data)
-    };
-    fetchRepos()
-  },[user])
+      };
+      fetchRepos()
+  };
 
+  const handleChange=(e)=>{
+    setUser(e.target.value)
+  }
+  const handleSubmit = () => {
+    fetchData();
+  }
   return (
     <div className="app">
+      <div>
+        <p htmlFor='username'>User Name :</p>
+        <input type="text" name='username' onChange={handleChange} />
+        <Button colorScheme="teal" size='md' onClick={handleSubmit} >Submit</Button>
+      </div>
       <h2>Repositories</h2>
       {items && (<>
         <div className="repos">
