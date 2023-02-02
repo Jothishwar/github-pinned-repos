8d8f539e950fcabe8dc4e937096a9fe09f0f2546
jothishwar <jothishwarvvr@gmail.com>
2023-02-02 21:53:48 +0530

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.0bd423ef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgithub_user=self.webpackChunkgithub_user||[]).push([[5539],{2689:function(e){function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.0bd423ef.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.ad0eaee8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgithub_user=self.webpackChunkgithub_user||[]).push([[3140],{7761:function(e){function a(e){e.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}e.exports=a,a.displayName="hsts",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.ad0eaee8.chunk.js.map
